e5bb0d55914fab78b06263ba0d8874300b433ec3
Lionel <lionel@slideshare.com>
2015-08-03 16:34:34 -0700

    initial commit lesson1

---
+++ Lesson1/calculator.rb
@@ -0,0 +1,52 @@
+# A basic cli based calculator
+#  performs computation on two integers
+
+def get_num
+  puts "Enter a number: "
+  num = gets.chomp
+  until num =~ /^\d+$/
+    puts "Nope.  Please enter a valid integer"
+    num = gets.chomp
+  end
+  num
+end
+
+def get_operand
+  puts "Enter an operand"
+  puts "1. +\n2. -\n3. *\n4. /\n"
+  operand = gets.chomp.to_i
+  until (1..4).include?(operand)
+    puts "Nope.  Please enter integer between 1-4"
+    operand = gets.chomp.to_i
+  end
+  operand
+end
+
+
+loop do
+  num1 = get_num
+  operand = get_operand
+  num2 = get_num
+
+  if operand == 4 && num2.to_i == 0
+    puts "Explosion to Infinity, try again"
+    next
+  end
+
+  answer = case operand
+    when 1
+      num1.to_i + num2.to_i
+    when 2
+      num1.to_i - num2.to_i
+    when 3
+      num1.to_i * num2.to_i
+    when 4
+      num1.to_f / num2.to_f
+  end
+
+  puts "The answer is: #{answer}\n\n"
+  puts "Want another?"
+  yes_or_no = gets.chomp
+  break unless yes_or_no =~ /^y+e*s*$/i
+end
+
---
+++ README.md
@@ -0,0 +1,8 @@
+Tealeaf Academy First Course
+Introduction to Ruby and Web Development
+=====================
+
+Programming Assignments
+
+https://www.gotealeaf.com/courses/94c34e0c/home
+
